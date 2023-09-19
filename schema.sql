ef25d268d78b37c7fd8ee8e8006f815ae1a4510c
Lawmsangi <ddarkphoenixx22@gmail.com>
2023-09-19 16:14:34 +0530

    Add multiple tables

--- schema.sql
+++ schema.sql
@@ -13,3 +13,47 @@
 -- Add the "species" column to the "animals" table
 ALTER TABLE animals
 ADD COLUMN species VARCHAR(150);
+
+
+--Create a table named owners with the following columns:
+CREATE TABLE owners (
+    id SERIAL PRIMARY KEY,
+    full_name VARCHAR(255),
+    age INTEGER
+);
+
+--Create a table named species with the following columns
+CREATE TABLE species (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+
+--Modify your inserted animals so it includes the species_id value
+ALTER TABLE animals
+RENAME COLUMN id TO animal_id;
+
+ALTER TABLE animals
+ADD id SERIAL;
+
+UPDATE animals
+SET id = animal_id;
+
+ALTER TABLE animals
+DROP COLUMN animal_id;
+
+ALTER TABLE animals
+DROP COLUMN species;
+
+ALTER TABLE animals
+ADD COLUMN species_id INTEGER;
+
+ALTER TABLE animals
+ADD CONSTRAINT fk_1
+FOREIGN KEY (species_id) REFERENCES species (id);
+
+ALTER TABLE animals
+ADD COLUMN owner_id INTEGER;
+
+ALTER TABLE animals
+ADD CONSTRAINT fk_2
+FOREIGN KEY (owner_id) REFERENCES owners (id);
